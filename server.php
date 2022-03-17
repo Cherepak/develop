a0fcc022853017591acc9998b5f9622bf48ecd56
Your Name <perec9595@mail.ru>
2022-03-17 18:41:52 +0300

    17.03.2022

---
+++ server.php
@@ -0,0 +1,15 @@
+<?php
+
+$email = $_POST["email"];
+$numberPhone = $_POST["numberPhone"];
+$name = $_POST["name"];
+
+if(!isset($email) and !isset($numberPhone) and ($name)) {
+
+} else {
+    
+}
+
+
+
+?>
