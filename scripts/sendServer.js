a675fa49be28ac01cb75e4d476b6f58fe79775cf
Дмитрий Черепаха <perec9595@mail.ru>
2022-05-12 16:49:57 +0300

    12.05.2022

--- scripts/sendServer.js
+++ scripts/sendServer.js
@@ -1,8 +1,26 @@
-// let button = document.getElementById("send");
+let button = document.getElementById("send");
 
-// button.addEventListener("click", () => {
-//     let result = fetch("./server.php")
-//     .then(result => {
-//         console.log(result)
-//     })
-// });
+button.addEventListener("click", () => {
+    let name  = document.getElementById("name")
+    let email = document.getElementById("email")
+    let numberPhone = document.getElementById("numberPhone");
+
+    fetch("./server.php", {
+        method: "POST",
+        body: {
+            name:name,
+            email:email,
+            numberPhone:numberPhone
+        }
+    })
+    .then(result => {
+        return result.json();
+    })
+    .then(data => {
+        if(data) {
+            alert("Данные отправленны, с Вами скоро свжутся по Вашему запросу")
+        } else {
+            alert("Возникла ошибка" + data)
+        }
+    })
+});
--- server.php
+++ server.php
@@ -5,9 +5,12 @@ $numberPhone = $_POST["numberPhone"];
 $name = $_POST["name"];
 
 if(!isset($email) and !isset($numberPhone) and ($name)) {
+    if(mail("info-develop@yandex.ru", "Запрос", "Имя: " $name "Номер: " $numberPhone "Почта: " $email)) {
+        echo(true)
+    }
 
 } else {
-    
+    echo("Не заполнены все данные")
 }
 
 
